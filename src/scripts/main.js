c0f395df3e956cc6879efd9c53f0131e8276945b
Pedro Lozano <peterlozano@gmail.com>
2014-09-27 10:33:52 +0200

    Remove commented code.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,8 +9,6 @@ var bespoke = require('bespoke'),
   progress = require('bespoke-progress'),
   forms = require('bespoke-forms');
 
-//$ = require('jquery');
-
 // Bespoke.js
 var deck = bespoke.from('article', [
   cube(),
@@ -27,26 +25,3 @@ var deck = bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-
-/*
-deck.on('activate', function(event) {
-  var id = event.slide.id;
-
-  var code = '';
-  switch (id) {
-
-    default:
-  }
-
-  if (code != '') {
-    $(event.slide).append('<div class="slide-code">' + code + '</div>');
-  }
-
-  return false;
-});
-
-deck.on('deactivate', function(event) {
-  $(event.slide).children().filter('.slide-code').remove();
-  return false;
-});
-*/
